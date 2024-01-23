ff3eead0f2edbf8b42e24efb6de99a7fbe302e45
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-01-23 20:27:19 +0100

    Add module stock_release_channel_plan_shipment_lead_time

---
+++ stock_release_channel_plan_shipment_lead_time/models/stock_release_channel.py
@@ -0,0 +1,36 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models
+
+
+class StockReleaseChannel(models.Model):
+    _inherit = "stock.release.channel"
+
+    delivery_weekday_ids = fields.Many2many(
+        "time.weekday",
+        "release_channel_delivery_weekday_rel",
+        "channel_id",
+        "weekday_id",
+    )
+    preparation_weekday_ids = fields.Many2many(
+        "time.weekday",
+        compute="_compute_preparation_weekday_ids",
+        readonly=True,
+        store=True,
+    )
+
+    @api.depends("delivery_weekday_ids", "shipment_lead_time")
+    def _compute_preparation_weekday_ids(self):
+        for channel in self:
+            weekday_names = []
+            delivery_weekdays = channel.delivery_weekday_ids
+            for wd in delivery_weekdays:
+                wd_minus_lead = int(wd.name) - channel.shipment_lead_time
+                if wd_minus_lead < 0:
+                    while not 0 <= wd_minus_lead < 7:
+                        wd_minus_lead += 7
+                weekday_names.append(str(wd_minus_lead))
+            channel.preparation_weekday_ids = self.env["time.weekday"].search(
+                [("name", "in", weekday_names)]
+            )
