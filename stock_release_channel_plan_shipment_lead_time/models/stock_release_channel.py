40ebea4af2ed2070d75844f06a98435facd0a2e2
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-01-25 20:33:36 +0100

    fixup! fixup! Add module stock_release_channel_plan_shipment_lead_time

--- stock_release_channel_plan_shipment_lead_time/models/stock_release_channel.py
+++ stock_release_channel_plan_shipment_lead_time/models/stock_release_channel.py
@@ -23,15 +23,34 @@ class StockReleaseChannel(models.Model):
 
     @api.depends("delivery_weekday_ids", "shipment_lead_time")
     def _compute_preparation_weekday_ids(self):
+        """Preparation weekdays are delivery weekdays - lead time"""
         for channel in self:
             weekday_names = []
             delivery_weekdays = channel.delivery_weekday_ids
             for wd in delivery_weekdays:
-                wd_minus_lead = int(wd.name) - channel.shipment_lead_time
-                if wd_minus_lead < 0:
-                    while not 0 <= wd_minus_lead < 7:
-                        wd_minus_lead += 7
-                weekday_names.append(str(wd_minus_lead))
+                if channel.warehouse_id.calendar_id:
+                    # To consider days off in the calendar, we look for the next date
+                    #  from today on whose weekday matches with current delivery weekday.
+                    # We then deduce the lead time using the calendar to find a weekday
+                    #  that is open.
+                    # It's not perfect, but it's less wrong than only deducing the days
+                    #  without consideration of closed days on weekends
+                    date_from = fields.Date.today()
+                    while date_from.weekday() != int(wd.name):
+                        date_from = fields.Date.add(date_from, days=1)
+                    date_from_minus_lead = channel.warehouse_id.calendar_id.plan_days(
+                        -channel.shipment_lead_time,
+                        fields.Datetime.to_datetime(date_from),
+                    )
+                    weekday_names.append(str(date_from_minus_lead.weekday()))
+                else:
+                    # If we don't have a calendar, deduce lead time and look
+                    #  for corresponding weekday
+                    wd_minus_lead = int(wd.name) - channel.shipment_lead_time
+                    if wd_minus_lead < 0:
+                        while not 0 <= wd_minus_lead < 7:
+                            wd_minus_lead += 7
+                    weekday_names.append(str(wd_minus_lead))
             channel.preparation_weekday_ids = self.env["time.weekday"].search(
                 [("name", "in", weekday_names)]
             )
